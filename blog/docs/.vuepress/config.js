578490d956c5e6a47b6a18e7e22820049f80537a
PikachuHy <pikachuhy@163.com>
2018-11-21 17:57:24 +0800

    add blog

---
+++ blog/docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: '大黄老鼠的番茄酱',
+    description: '博客',
+    plugins: [
+        '@vuepress/back-to-top',
+        '@vuepress/medium-zoom'
+    ],
+    themeConfig: {
+        navbar: false,
+        sidebar: 'auto',
+        displayAllHeaders: true,
+        sidebarDepth: 2
+    }
+}
